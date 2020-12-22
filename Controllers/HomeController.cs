d2426ba7cfd322b1c6b862e6a69b966179d6bf01
liuyangxingyu <2647114409@qq.com>
2020-12-22 11:58:17 +0800

    添加项目文件。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using Models;
+namespace dome.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult QueryAll(int pageindex, int pagesize) {
+            return Json(StudentManager.QueryAll(pageindex, pagesize), JsonRequestBehavior.AllowGet);
+        }
+        public int GetCount()
+        {
+            return StudentManager.GetCount();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dome.MvcApplication" Language="C#" %>
